f3f9b11d4840cbfa2abfd54f583da8889350412f
BRUNO HENRIQUE BALSAN <00001109645764sp@aluno.educacao.sp.gov.br>
2026-03-30 08:42:52 -0300

    teste de PWAs

--- SEMANA6/src/main.js
+++ SEMANA6/src/main.js
@@ -1,6 +1,4 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import store from './store/index.js'
+createApp(App).use(store).mount('#app')
